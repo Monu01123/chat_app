ce30f878c23e1380afc2b8679c2cd767fb4ebc35
Monu01123 <monumeena0112@gmail.com>
2024-12-18 22:37:43 +0530

    created routes and controllers for auth and messages

--- backend/models/Message.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const messageSchema = Schema({
-  sender: { type: Schema.Types.ObjectId, ref: 'User' },
-  content: { type: String, required: true },
-  chat: { type: Schema.Types.ObjectId, ref: 'Chat' },
-}, { timestamps: true });
-
-export default model('Message', messageSchema);
